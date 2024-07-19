812eac49992a00de7bb2411f67d16544c463f5fb
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-19 08:05:27 +0200

    making my counting sheeps and wolf function

---
+++ app.js
@@ -0,0 +1,19 @@
+const countingSheepsAndWolves = (sheeps) =>
+{    let sheepCount = 0;
+
+    for (let i = 0; i < sheeps.length; i++) {
+        if (sheeps[i] === true) {
+            sheepCount += 1;
+        }
+    }
+
+    if (sheepCount === 0) {
+        return "wolves eaten sheeps!";
+    }
+     else {
+        return `Number of sheeps: ${sheepCount}`;
+    }
+};
+
+
+console.log(countingSheepsAndWolves([true, true, true, true, false, false]));
